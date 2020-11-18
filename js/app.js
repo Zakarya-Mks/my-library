10b008bb55b788b0da236029a8a729b87a4b2e10
Zakarya-Mks <zakaaryaaa@gmail.com>
2020-11-18 16:42:34 +0100

    feat: remove book from localStorage and ui based on book_id when remove button clicked

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
-let books = [
+let book_collection = [
   {
+    id: 1,
     title: 'a song of ice and fire',
     author: 'george r.r martin',
     'nbr-of-pages': '649',
@@ -8,6 +9,7 @@ let books = [
     'read-status': true,
   },
   {
+    id: 2,
     title: 'a song of ice and fire 3',
     author: 'george r.r martin',
     'nbr-of-pages': '649',
@@ -16,6 +18,7 @@ let books = [
     'read-status': false,
   },
   {
+    id: 3,
     title: 'a song of ice and fire 3',
     author: 'george r.r martin',
     'nbr-of-pages': '649',
@@ -24,6 +27,7 @@ let books = [
     'read-status': true,
   },
   {
+    id: 4,
     title: 'a song of ice and fire 4',
     author: 'george r.r martin',
     'nbr-of-pages': '649',
@@ -33,15 +37,16 @@ let books = [
   },
 ];
 
-// (function add_testing_books() {
-//   localStorage.setItem(books, JSON.stringify(books));
-// })();
+// add books to the localStorage
+(function add_testing_books() {
+  localStorage.setItem('book_collection', JSON.stringify(book_collection));
+})();
 
 function add_book_to_ui(book) {
   let book_template_html = `
   <div class="single_book ${
     book['read-status'] ? 'read' : 'not-read'
-  }" id="book-01">
+  }" id="book-${book.id}">
   <span class="material-icons remove-book"> highlight_off </span>
   <h3 class="book-title">${book.title}</h3>
   <span class="book-author">
@@ -61,12 +66,46 @@ function add_book_to_ui(book) {
     .insertAdjacentHTML('beforeend', book_template_html);
 }
 
+function remove_book_from_ui(book_id) {
+  document
+    .querySelector('.row')
+    .removeChild(document.querySelector(`#book-${book_id}`));
+}
+
+function remove_book_from_localeStorage(rm_book_id) {
+  let temp_books_collection = JSON.parse(
+    localStorage.getItem('book_collection')
+  ).filter((book) => {
+    if (book.id != rm_book_id) {
+      return book;
+    }
+  });
+
+  localStorage.setItem(
+    'book_collection',
+    JSON.stringify(temp_books_collection)
+  );
+}
+
 window.addEventListener('load', (e) => {
-  if (localStorage.getItem(books)) {
-    let saved_books = JSON.parse(localStorage.getItem(books));
-    console.log(saved_books);
-    saved_books.forEach((book) => {
+  if (localStorage.getItem('book_collection')) {
+    let saved_books_collection = JSON.parse(
+      localStorage.getItem('book_collection')
+    );
+
+    saved_books_collection.forEach((book) => {
       add_book_to_ui(book);
     });
+  }
+});
+
+document.querySelector('.row').addEventListener('click', (e) => {
+  if (e.target.classList.contains('remove-book')) {
+    let book_id = e.target.parentNode.id.substring(
+      e.target.parentNode.id.indexOf('-') + 1
+    );
+
+    remove_book_from_localeStorage(book_id);
+    remove_book_from_ui(book_id);
   }
 });
